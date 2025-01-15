95dbe6f2410626d5cf7e3d9b479a8293b2b5d9e6
delee <delee.dev@gmail.com>
2025-01-15 23:30:39 +0900

    test: 유저 단위 테스트 추가

---
+++ src/test/java/kr/hhplus/be/server/domain/user/UserServiceTest.java
@@ -0,0 +1,41 @@
+package kr.hhplus.be.server.domain.user;
+
+import kr.hhplus.be.server.global.exception.DomainException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+
+    @Nested
+    @DisplayName("유저 조회")
+    class GetUserTest {
+        @Test
+        void 존재하지_않는_회원을_조회할_때_예외가_발생한다() {
+            // given
+            long nonExistentUserId = 99L;
+
+            when(userRepository.findById(nonExistentUserId))
+                .thenReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> userService.getUser(nonExistentUserId))
+                    .isInstanceOf(DomainException .class)
+                    .hasMessage(UserErrorCode.USER_NOT_FOUND.getMessage());
+        }
+    }
+}

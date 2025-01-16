8c2618587c9e392cf30e38e976a0e0b88dddd39a
delee <delee.dev@gmail.com>
2025-01-17 03:45:15 +0900

    test: E2E 테스트 데이터 입력 방법 변경

--- src/test/java/kr/hhplus/be/server/api/coupon/CouponControllerE2ETest.java
+++ src/test/java/kr/hhplus/be/server/api/coupon/CouponControllerE2ETest.java
@@ -3,13 +3,35 @@ package kr.hhplus.be.server.api.coupon;
 import io.restassured.RestAssured;
 import kr.hhplus.be.server.api.BaseE2ETest;
 import kr.hhplus.be.server.api.coupon.dto.IssueCouponRequest;
+import kr.hhplus.be.server.domain.coupon.model.Coupon;
+import kr.hhplus.be.server.domain.user.model.User;
+import kr.hhplus.be.server.infrastructure.coupon.persistence.CouponJpaRepository;
+import kr.hhplus.be.server.infrastructure.user.persistence.UserJpaRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import static kr.hhplus.be.server.fixture.integration.Fixture.coupon;
+import static kr.hhplus.be.server.fixture.integration.Fixture.user;
 import static org.hamcrest.Matchers.equalTo;
 
-@Sql("data.sql")
+@Sql("/clear.sql")
 public class CouponControllerE2ETest extends BaseE2ETest {
+    @Autowired
+    private CouponJpaRepository couponJpaRepository;
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @BeforeEach
+    void setUp() {
+        User user = user();
+        userJpaRepository.saveAndFlush(user);
+
+        Coupon coupon = coupon(100);
+        couponJpaRepository.saveAndFlush(coupon);
+    }
+
     @Test
     void 쿠폰_발행_E2E_테스트() {
         // when
--- src/test/java/kr/hhplus/be/server/api/point/PointControllerE2ETest.java
+++ src/test/java/kr/hhplus/be/server/api/point/PointControllerE2ETest.java
@@ -3,13 +3,35 @@ package kr.hhplus.be.server.api.point;
 import io.restassured.RestAssured;
 import kr.hhplus.be.server.api.BaseE2ETest;
 import kr.hhplus.be.server.api.point.dto.ChargePointRequest;
+import kr.hhplus.be.server.domain.point.model.Point;
+import kr.hhplus.be.server.domain.user.model.User;
+import kr.hhplus.be.server.infrastructure.point.persistence.PointJpaRepository;
+import kr.hhplus.be.server.infrastructure.user.persistence.UserJpaRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import static kr.hhplus.be.server.fixture.integration.Fixture.point;
+import static kr.hhplus.be.server.fixture.integration.Fixture.user;
 import static org.hamcrest.Matchers.equalTo;
 
-@Sql("data.sql")
+@Sql("/clear.sql")
 public class PointControllerE2ETest extends BaseE2ETest {
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+    @Autowired
+    private PointJpaRepository pointJpaRepository;
+
+    @BeforeEach
+    void setUp() {
+        User user = user();
+        userJpaRepository.saveAndFlush(user);
+
+        Point point = point(user, 1_000_000L);
+        pointJpaRepository.saveAndFlush(point);
+    }
+
     @Test
     void 포인트_조회_E2E_테스트() {
         // given
@@ -24,7 +46,7 @@ public class PointControllerE2ETest extends BaseE2ETest {
             .then()
                 .statusCode(200)
                 .body("userId", equalTo(1))
-                .body("userName", equalTo("이다은"))
+                .body("userName", equalTo("김철수"))
                 .body("balance", equalTo(1000000));
     }
 
@@ -45,7 +67,7 @@ public class PointControllerE2ETest extends BaseE2ETest {
             .then()
                 .statusCode(200)
                 .body("userId", equalTo(1))
-                .body("userName", equalTo("이다은"))
+                .body("userName", equalTo("김철수"))
                 .body("balance", equalTo(1010000));
     }
 }

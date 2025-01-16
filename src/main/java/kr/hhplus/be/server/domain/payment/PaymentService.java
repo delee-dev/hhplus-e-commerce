38364f54d009a8b5e3ec0cab9517a5052d92151d
delee <delee.dev@gmail.com>
2025-01-17 00:40:36 +0900

    fix: 서비스 레이어에 누락된 트랜잭션 어노테이션 추가

--- src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
+++ src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
@@ -3,16 +3,19 @@ package kr.hhplus.be.server.domain.payment;
 import kr.hhplus.be.server.domain.payment.model.Payment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
 
+    @Transactional
     public Payment createPayment(Long orderId, Long totalAmount) {
         return paymentRepository.save(new Payment(orderId, totalAmount));
     }
 
+    @Transactional
     public Payment getPaymentByOrderIdWithLock(Long orderId) {
         return paymentRepository.getPaymentByOrderIdWithLock(orderId);
     }

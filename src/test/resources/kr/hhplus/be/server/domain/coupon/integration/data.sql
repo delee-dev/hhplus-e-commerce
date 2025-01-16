40bfc2af7c4bfabd58282b957a4dd1c9972843b8
delee <delee.dev@gmail.com>
2025-01-17 00:45:42 +0900

    test: 쿠폰 통합 테스트 추가 및 테스트 데이터 입력 방법 변경

--- src/test/resources/kr/hhplus/be/server/domain/coupon/integration/data.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE coupons;
-
-INSERT INTO coupons (id, name, discount_type, discount_amount, min_order_amount, max_discount_amount, valid_from, valid_until, total_quantity, created_at, updated_at) VALUES
-    (1, '신규가입 할인', 'FIXED_AMOUNT', 10000, 50000, 10000, DATE_SUB(NOW(), INTERVAL 1 MONTH), DATE_ADD(NOW(), INTERVAL 1 MONTH), 1000, DATE_SUB(NOW(), INTERVAL 1 MONTH), DATE_SUB(NOW(), INTERVAL 1 MONTH));
